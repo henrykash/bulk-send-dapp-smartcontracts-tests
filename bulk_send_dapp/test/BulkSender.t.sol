45feb56caab2ca66e224ba2d7edc8336223b5595
henrykash <henrykariuki003@gmail.com>
2022-11-10 15:25:49 +0300

    testing complete🔥🔥🔥🔥🔥

--- bulk_send_dapp/test/BulkSender.t.sol
+++ bulk_send_dapp/test/BulkSender.t.sol
@@ -10,22 +10,25 @@ import {TokenERC20} from "./utils/TokenERC20.sol";
 
 contract ContractTest is Test {
     uint256 mainnetFork;
+
     string internal constant MAINNET_FORK_URL =
         "https://eth.getblock.io/cd527522-6486-4d11-9c35-b9f498dd0e9c/mainnet/";
 
     BulkSender bulkSender;
     TokenERC20 token;
 
-    //variables for testing
+    //impersonating this address, setting to admin
     address public defaultAdmin = 0x266fedED59399AFC982EEa44724fCa7Ba31C054f;
-    address public MUFASA = 0x210f4F7a092CCdc3487B8dAB8e317A6E29aeA720;
 
     function setUp() public {
         vm.createFork(MAINNET_FORK_URL);
         vm.selectFork(mainnetFork);
 
         vm.startPrank(defaultAdmin);
+
+        //instatiate contracts
         bulkSender = new BulkSender();
+        token = new TokenERC20("Test", "TST");
         vm.stopPrank();
     }
 
@@ -36,22 +39,26 @@ contract ContractTest is Test {
     function testbatchTransfer() public {
         vm.startPrank(defaultAdmin);
 
+        //initialize the defaultAdmin as the admin of the bulkSender
         bulkSender.initialize(defaultAdmin);
 
-        //assert that the balance of the bulkSender is 0 before transfer od tokens to it
-        assertEq(token.balanceOf(address(bulkSender)), 0);
-        token.mintTo(address(bulkSender), 1000000);
+        //assert that the balance of the default is 0 before transfer of tokens to it
+        assertEq(token.balanceOf(address(defaultAdmin)), 0);
+        token.mintTo(address(defaultAdmin), 1000000000000000000);
 
-        //assert that the balance of the bulkSender is 1000 after transfer od tokens to it
-        assertEq(token.balanceOf(address(bulkSender)), 1000000);
+        //assert that the balance of the defaultAdmin is 1000000000000000000 after transfer of tokens to it
+        assertEq(token.balanceOf(address(defaultAdmin)), 1000000000000000000);
+
+        //approve the bulkSender to spend the tokens
+        token.approve(address(bulkSender), 1000000000000000000);
 
         address[] memory to = new address[](2);
         to[0] = 0x8A9c4dfe8b9D8962B31e4e16F8321C44d48e246E;
         to[1] = 0x11eDedebF63bef0ea2d2D071bdF88F71543ec6fB;
 
         uint256[] memory amounts = new uint256[](2);
-        amounts[0] = 10000000000;
-        amounts[1] = 10000000000;
+        amounts[0] = 10000000;
+        amounts[1] = 10000000;
 
         bulkSender.batchTransfer(IERC20Upgradeable(token), to, amounts);
         vm.stopPrank();
@@ -59,16 +66,23 @@ contract ContractTest is Test {
 
     function testwithdrawToken() public {
         vm.startPrank(defaultAdmin);
+
+        //initialize the defaultAdmin as the admin of the bulkSender
+        bulkSender.initialize(defaultAdmin);
+
         //assert that the balance of the bulkSender is 0 before transfer od tokens to it
         assertEq(token.balanceOf(address(bulkSender)), 0);
         token.mintTo(address(bulkSender), 1000);
 
         //assert that after trasfering tokens to the bulksender the balance is 1000
         assertEq(token.balanceOf(address(bulkSender)), 1000);
+
+        //withdraw the tokens from the bulkSender
         bulkSender.widthrawToken(IERC20Upgradeable(token), 500);
 
         //assert that after withdrawing 500 tokens the balance is 500 for buksender contract
         assertEq(token.balanceOf(address(bulkSender)), 500);
+
         //assert that the balance of the default admin is 500
         assertEq(token.balanceOf(defaultAdmin), 500);
 
